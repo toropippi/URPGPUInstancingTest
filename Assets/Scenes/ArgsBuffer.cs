f3285655311fffd24c642f0364f836e5acc06958
toropippi <44022497+toropippi@users.noreply.github.com>
2022-06-10 14:23:16 +0900

    _

---
+++ Assets/Scenes/ArgsBuffer.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Sandbox
+{
+    public class ArgsBuffer
+    {
+        uint[] args = new uint[] { 0, 0, 0, 0, 0 };
+        ComputeBuffer buffer;// = new ComputeBuffer(1, sizeof(uint) * 5, ComputeBufferType.IndirectArguments);
+
+        public ComputeBuffer Buffer => buffer;
+
+        public void SetData(Mesh mesh, int subMeshIndex, uint instanceCount)
+        {
+            buffer = new ComputeBuffer(1, sizeof(uint) * 5, ComputeBufferType.IndirectArguments);
+            args[0] = mesh.GetIndexCount(subMeshIndex);
+            args[1] = instanceCount;
+            args[2] = mesh.GetIndexStart(subMeshIndex);
+            args[3] = mesh.GetBaseVertex(subMeshIndex);
+
+            buffer.SetData(args);
+        }
+
+    }
+}
---
+++ Assets/Scenes/Particles.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+namespace Sandbox
+{
+
+    public struct Particle
+    {
+        public Vector3 position;
+
+        public static Particle Create()
+        {
+            return new Particle
+            {
+                position = new Vector3(Random.Range(-7.0f, 7.0f), Random.Range(-4.0f, 9.0f), Random.Range(-5.0f, 5.0f))
+            };
+        }
+    }
+
+
+    public class Particles : MonoBehaviour
+    {
+        [SerializeField] Mesh mesh;
+        [SerializeField] Material material;
+
+        ComputeBuffer particleBuffer;
+        Bounds bounds;
+        const int instanceCount = 100;
+        ArgsBuffer argsBuffer;
+        void Start()
+        {
+            bounds = new Bounds(Vector3.zero, Vector3.one * 1000);
+            argsBuffer = new ArgsBuffer();
+            argsBuffer.SetData(mesh, 0, instanceCount);
+
+            particleBuffer = new ComputeBuffer(instanceCount, 12);
+
+            var particles = new Particle[instanceCount];
+
+            for (var i = 0; i < instanceCount; ++i)
+            {
+                particles[i] = Particle.Create();
+            }
+
+            particleBuffer.SetData(particles);
+
+            material.SetBuffer("_Particles", particleBuffer);            
+        }
+
+        private void Update()
+        {
+            Graphics.DrawMeshInstancedIndirect(mesh, 0, material, bounds, argsBuffer.Buffer);
+        }
+
+        void OnDestroy()
+        {
+            particleBuffer.Release();
+        }
+    }
+}
+
